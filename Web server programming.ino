ad20a56b7fed67f12a269b048cd31673aceb9dce
Anshuman Srivastava <anshumansrivastava80@gmail.com>
2021-05-18 15:13:20 +0530

    Code style changes

--- Web server programming.ino
+++ Web server programming.ino
@@ -1,12 +1,11 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
- 
+
 const char* ssid = "nishant";
 const char* password = "nishant1";
- 
+
 ESP8266WebServer server(80);   // server at port 80 (http port)
- 
 
 float data;
 int PUMPin = 16;
@@ -17,40 +16,35 @@ String sendpage(){
   Serial.println(data);
   delay(500);
   String page = "";
-  page ="<!DOCTYPE html>";
-  page +="<html>";
-  page +="<body>";
-  page +="<center>";
-  page +="<h1>National Institute of Technology<br>Patna</h1>";
-  page +="<h1><br></h1>";
-  page +="<h2>MAJOR PROJECT (GROUP-26)</h2>";
-  page +="<h2>Smart Irrigation System</h2>";
-  page +="<h4>PUMP</h4>";
-  page +="<p><a href=\"PUMP1on\"><button>ON</button></a>&nbsp;<a href=\"PUMP1off\"><button>OFF</button></a></p>";
-  page +="<h2>Soil moisture(%)</h2>";
-  page +="<h4>"+String(data)+"</h4>";
-  page +="</center>";
-  page +="<h4 align=\"center\"><meter value="+String(data)+" min=0 max=100></meter></h4>";
-  page +="</body>";
-  page +="</html>";
+  page = "<!DOCTYPE html>"
+    "<html>"
+    "<body>"
+    "<center>"
+    "<h1>National Institute of Technology<br>Patna</h1>"
+    "<h1><br></h1>"
+    "<h2>MAJOR PROJECT (GROUP-26)</h2>"
+    "<h2>Smart Irrigation System</h2>"
+    "<h4>PUMP</h4>"
+    "<p><a href=\"PUMP1on\"><button>ON</button></a>&nbsp;<a href=\"PUMP1off\"><button>OFF</button></a></p>"
+    "<h2>Soil moisture(%)</h2>"
+    "<h4>"+String(data)+    "</h4>"
+    "</center>"
+    "<h4 align=\"center\"><meter value="+String(data)+" min=0 max=100></meter></h4>"
+    "</body>"
+    "</html>";
 
   return page;
 }
-   
-
 
 void setup(void){
-
-
-  
   pinMode(A0, INPUT);
   pinMode(PUMPin, OUTPUT);
-  
+
   delay(1000);
   Serial.begin(115200);
   WiFi.begin(ssid, password); //begin WiFi connection
   Serial.println("");
-  
+
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -72,6 +66,7 @@ void setup(void){
     digitalWrite(PUMPin, HIGH);
     delay(500);
   });
+
   server.on("/PUMP1off", [](){
     String page = sendpage();
     server.send(200, "text/html", page);
@@ -84,6 +79,5 @@ void setup(void){
 }
  
 void loop(void){
-
   server.handleClient();
 }
